e1a0e745e1fa62bda5b38650bf5113d92859ef17
nek07 <abylaiooaooa@mail.ru>
2023-10-24 04:05:12 +0600

    bootstrap packages

--- script.js
+++ script.js
@@ -59,3 +59,58 @@ const now = new Date();
     document.getElementById("date").innerText = day+" "+month.slice(0,3)+", "+dayOfWeek;
 }
 setDate()
+
+function validateFeedBackForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const comment = document.getElementById("comment").value;
+
+    if (name === "" || email === "" || comment === "") {
+        alert("Please fill in all the required fields.");
+        return false;
+    }
+
+    return true;
+}
+
+function validateLoginForm() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username.trim() === "" || password.trim() === "") {
+        alert("Please enter both a username and a password.");
+        return false;
+    }
+
+    // You can add more complex validation rules here if needed.
+
+    return true;
+}
+
+
+let interval;
+let remainingTime = 0;
+
+function startTimer() {
+    clearInterval(interval);
+    const setTime = parseInt(document.getElementById("setTime").value);
+    if (!isNaN(setTime) && setTime > 0) {
+        remainingTime = setTime;
+        interval = setInterval(updateTimer, 1000);
+    } else {
+        alert("Please enter a valid time in seconds.");
+    }
+}
+
+function updateTimer() {
+    if (remainingTime <= 0) {
+        clearInterval(interval);
+        document.getElementById("timer").textContent = "00:00";
+    } else {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        const formattedTime = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        document.getElementById("timer").textContent = formattedTime;
+        remainingTime--;
+    }
+}

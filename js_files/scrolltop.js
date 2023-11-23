3b6248c4a9fe6ffd4c49b7ba69fb81cd6f578fb3
nek07 <abylaiooaooa@mail.ru>
2023-11-23 23:25:36 +0600

    video page and modal box

--- js_files/scrolltop.js
+++ js_files/scrolltop.js
@@ -1,21 +1,38 @@
-    // Get the button
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {
-      scrollFunction();
-    };
-    
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-      } else {
-        scrollToTopBtn.style.display = "none";
-      }
-    }
-    
-    // Scroll to the top of the document when the button is clicked
-    function scrollToTop() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Scroll to the top of the document when the button is clicked
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Function to open the modal
+function openModal() {
+  document.getElementById("myModal").style.display = "flex";
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Close the modal if the overlay is clicked
+document.addEventListener('click', function (event) {
+  if (event.target === document.getElementById("myModal")) {
+    closeModal();
+  }
+});

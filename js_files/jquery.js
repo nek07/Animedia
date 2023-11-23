dd6e15b161126ce74023d1e6202438658ec3a08f
nek07 <abylaiooaooa@mail.ru>
2023-11-23 23:35:13 +0600

    video debug

--- js_files/jquery.js
+++ js_files/jquery.js
@@ -133,23 +133,6 @@ function fixStepIndicator(n) {
   //... and adds the "active" class to the current step:
   x[n].className += " active";
 }
-var myVideo = document.getElementById("video1");
 
-function playPause() {
-    if (myVideo.paused)
-        myVideo.play();
-    else
-        myVideo.pause();
-}
 
-function makeBig() {
-    myVideo.width = 560;
-}
 
-function makeSmall() {
-    myVideo.width = 320;
-}
-
-function makeNormal() {
-    myVideo.width = 420;
-} 
